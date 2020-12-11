d4a310d8e0de7bf718a65c5f4b85577613e17c5d
Loic <jeschonek.l@gmail.com>
2020-12-11 17:02:08 +0100

    Base fonctionnelle React

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World ! <br/> My name is Laurent.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> 
+       
+      </header>
+      <br className = "mt-5" />
+      <p>Trololo</p>
+      <br className = "mt-5" />
+    </div>
+
+
+  );
+}
+
+export default App;
